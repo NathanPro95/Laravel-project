9a55008c28c73cac431de486ee77f85c52d98d95
= <=>
2020-03-29 22:28:31 +0700

    Update insert Track Progress by CSV

--- app/Http/Controllers/ImportExcelController.php
+++ app/Http/Controllers/ImportExcelController.php
@@ -2,17 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use DB;
 use Illuminate\Http\Request;
 use App\models\ScheduleImport;
+use App\models\TrackProgress;
 use Maatwebsite\Excel\Facades\Excel;
 
 class ImportExcelController extends Controller
 {
     //
-    public function import() 
-    {
+	public function import() 
+	{
 
-        Excel::import(new ScheduleImport,request()->file('file'));
-        return redirect('/manageSchedule/schedule');
-    }
+		Excel::import(new ScheduleImport,request()->file('file'));
+		$dataProgress = DB::table('track_progress')->rightJoin('schedules', 'track_progress.schedules_id', '=', 'schedules.id')->get(['schedules.id', 'schedules.schedule_name', 'track_progress.handover_gorund', 'track_progress.handover_of_subpplies', 'track_progress.construction', 'track_progress.area',  'track_progress.image_handover_ground', 'track_progress.image_handover_supplies', 'track_progress.created_at', 'track_progress.updated_at','track_progress.schedules_id'])->toArray();
+		foreach ($dataProgress as $key => $valueProgress){
+			if ((array)$valueProgress->schedules_id != (array)$valueProgress->id){
+				$trackProgress = new TrackProgress;
+				$trackProgress['handover_gorund'] = Config('const.DEFAUTLTRACKPROGRESS');
+				$trackProgress['handover_of_subpplies'] = Config('const.DEFAUTLTRACKPROGRESS');
+				$trackProgress['construction'] = Config('const.DEFAUTLTRACKPROGRESS');
+				$trackProgress['schedules_id'] = ((array)$valueProgress->id)[0];
+				$trackProgress->save();
+			}
+		}
+		return redirect('/manageSchedule/schedule');
+	}
 }
