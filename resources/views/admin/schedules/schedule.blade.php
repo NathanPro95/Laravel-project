4d29c017a81a05625e2f7915ba6793a86ad29191
techvify.tuanna <tuanna@techvify.com.vn>
2020-04-03 09:39:42 +0700

    update .env file and resovle conflict

--- resources/views/admin/schedules/schedule.blade.php
+++ resources/views/admin/schedules/schedule.blade.php
@@ -33,21 +33,21 @@
                     </form>
                     <table id="schedule_table" class="table table-striped table-bordered" style="width:100%">
                         <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Contract date</th>
-                                <th>Valuable</th>
-                                <th>Number emp</th>
-                                <th>Plan</th>
-                                <th>Status</th>
-                                <th>End date</th>
-{{--                                @role('admin')--}}
-                                    <th>Process</th>
-{{--                                @endrole--}}
-                            </tr>
+                        <tr>
+                            <th>Tên công trình</th>
+                            <th>Ngày hợp đồng</th>
+                            <th>Giá trị HĐ(VNĐ)</th>
+                            <th>Số công nhân</th>
+                            <th>Kế hoạch</th>
+                            <th>Trạng thái</th>
+                            <th>Ngày hoàn thành</th>
+                            @role('admin')
+                            <th>Thao tác</th>
+                            @endrole
+                        </tr>
                         </thead>
                         <tbody>
-                            @foreach($schedules as $schedule)
+                        @foreach($schedules as $schedule)
                             <tr>
                                 <td>{{$schedule['schedule_name']}}</td>
                                 <td>{{$schedule['contract_date'] != null ? date('d-m-Y',strtotime($schedule['contract_date'])) : ""}}</td>
@@ -56,45 +56,32 @@
                                 <td>{{$schedule['construction_plan'] != null ? $schedule['construction_plan'] : ""}}</td>
                                 <td>{{$schedule['schedule_status']}}</td>
                                 <td>{{$schedule['end_date'] != null ? date('d-m-Y',strtotime($schedule['end_date'])) : ""}}</td>
-{{--                                @role('admin')--}}
-                                    <td>
-                                        <a href="{{route('schedule.edit',$schedule['id'])}}" style="float: left;margin-right: 5px;" title="Cập nhật"><i class="fa fa-edit"></i></a>
-                                        <form action="{{route('schedule.delete',$schedule['id'])}}" method="post">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button style="background: unset;border: unset;color: #f70707;" onclick="return confirm('Bạn có chắc chắn muốn xóa '+'{{$schedule['schedule_name']}}' + '?');" type="submit" title="Xóa"><i class="fa fa-trash"></i></button>
-                                        </form>
-                                    </td>
-{{--                                @endrole--}}
+                                @role('admin')
+                                <td>
+                                    <a href="{{route('schedule.edit',$schedule['id'])}}" style="float: left;margin-right: 5px;" title="Cập nhật"><i class="fa fa-edit"></i></a>
+                                    <form action="{{route('schedule.delete',$schedule['id'])}}" method="post">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button style="background: unset;border: unset;color: #f70707;" onclick="return confirm('Bạn có chắc chắn muốn xóa '+'{{$schedule['schedule_name']}}' + '?');" type="submit" title="Xóa"><i class="fa fa-trash"></i></button>
+                                    </form>
+                                </td>
+                                @endrole
                             </tr>
-                            @endforeach
+                        @endforeach
                         </tbody>
                         <tfoot>
-                            <tr>
-<<<<<<< Updated upstream
-                                <th>Tên công trình</th>
-                                <th>Ngày hợp đồng</th>
-                                <th>Giá trị HĐ(VNĐ)</th>
-                                <th>Số công nhân</th>
-                                <th>Kế hoạch</th>
-                                <th>Trạng thái</th>
-                                <th>Ngày hoàn thành</th>
-                                @role('admin')
-                                <th>Thao tác</th>
-                                @endrole
-=======
-                                <th>Name</th>
-                                <th>Contract date</th>
-                                <th>Valuable</th>
-                                <th>Number emp</th>
-                                <th>Plan</th>
-                                <th>Status</th>
-                                <th>End date</th>
-{{--                                @role('admin')--}}
-                                    <th>Process</th>
-{{--                                @endrole--}}
->>>>>>> Stashed changes
-                            </tr>
+                        <tr>
+                            <th>Tên công trình</th>
+                            <th>Ngày hợp đồng</th>
+                            <th>Giá trị HĐ(VNĐ)</th>
+                            <th>Số công nhân</th>
+                            <th>Kế hoạch</th>
+                            <th>Trạng thái</th>
+                            <th>Ngày hoàn thành</th>
+                            @role('admin')
+                            <th>Thao tác</th>
+                            @endrole
+                        </tr>
                         </tfoot>
                     </table>
                 </div>
